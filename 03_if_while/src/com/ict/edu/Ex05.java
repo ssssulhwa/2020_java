0c1ff88640a01e2fe2a65b154795c110f48341ce
ssssulhwa <snowflowerof@naver.com>
2020-05-08 13:06:23 +0900

    sulhwa

--- 03_if_while/src/com/ict/edu/Ex05.java
+++ 03_if_while/src/com/ict/edu/Ex05.java
@@ -1,7 +1,7 @@
 package com.ict.edu;
 
 public class Ex05 {
-	
+
 	public static void main(String[] args) {
 		// for문 : 정해진 규칙에 따라 실행문을 반복 처리하는 문
 		// for(초기식; 조건식; 증감식){
@@ -36,31 +36,123 @@ public class Ex05 {
 
 		}
 		System.out.println("==========================");
-		
+
 		// 0- 10까지 출력하기
 		for (int i = 0; i < 11; i++) {
 			System.out.println(i);
 		}
 		System.out.println("==========================");
-		
-			// 0-10 까지 짝수만 출력하기
-		for (int i = 0; i < 11; i = i+2) {
+
+		// 0-10 까지 짝수만 출력하기
+		for (int i = 0; i < 11; i = i + 2) {
 			System.out.println(i);
 		}
-		System.out.println("=========================="); 
+		System.out.println("==========================");
 		for (int i = 0; i < 11; i++) {
 			if (i % 2 == 0) {
-			System.out.println(i);
+				System.out.println(i);
+			}
+
+		}
+		System.out.println("==========================");
+		for (int i = 0; i < 11; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i);
+
+			}
+
+		}
+		// 0 - 50 까지 7의 배수 출력
+		for (int i = 0; i < 51; i++) {
+			if (i % 7 == 0) {
+				// 출력 후 한 줄 띄우기
+				System.out.println(i);
+			}
+		}
+		// a- g
+		for (char i = 'a'; i <= 'g'; i++) {
+			// 옆으로 출력하기, \t는 tab을 의미
+			System.out.print(i + "\t");
+		}
+		// \n은 한 줄 띄우기
+		System.out.println("가나\n다라");
+		// 한 줄 띄우기
+		System.out.println();
+		// 존재 하지 않는 메소드
+		// System.out.print();
+
+		// System.out.println() : 줄 바꾸기
+		// System.out.print() : 존재 안함
+		// System.out.println(변수나 데이터) : 출력 후 줄 바꾸기
+		// System.out.print(변수나 데이터) : 옆으로 출력하기
+
+		// 5단 출력하기
+		for (int i = 1; i < 10; i++) {
+			System.out.println("5 * " + 1 + " = " + (5 * i));
+		}
+		//
+		for (int i = 1; i < 5; i++) {
+			System.out.println("0 0 0 0");
+		}
+		System.out.println();
+
+		for (int i = 1; i < 17; i++) {
+			System.out.println("0");
+			if (i % 4 == 0) {
+				System.out.println();
+
+			}
+
+		}
+		// 누적합 (합계)
+		int sum = 0; // 이전 값을 기억시키는 변수
+		for (int i = 0; i < 11; i++) {
+			sum = sum + i; // i 의 누적합
+		}
+		System.out.println("누적합 : " + sum);
+
+		// 0-10 홀수의 합계
+
+		int odd = 0;
+		for (int i = 0; i < 11; i++) {
+			if (i % 2 == 1) {
+				odd = odd + i;
+			}
+		}
+		System.out.println("홀수의 합 : " + odd);
+
+		// 0-10 짝수의 합계
+		int even = 0;
+		for (int i = 0; i < 11; i++) {
+			if (1 % 2 == 0) {
+				even = even + i;
+
+			}
+		}
+		System.out.println("짝수의 합 :" + even);
+
+		// 0-10 홀수, 짝수의 합계
+		odd = 0;
+		even = 0;
+		for (int i = 0; i < 11; i++) {
+			if (i % 2 == 0) {
+				even = even + i ;
+				
+			} else {
+				odd = odd + 1 ;
+
 			}
 			
 		}
-		System.out.println("=========================="); 
-		for (int i = 0; i < 11; i++) {
-			if (i % 2 == 1) {
-			System.out.println(i);
-		
-	}
+		System.out.println("짝수의 합 :" + even);
+		System.out.println("홀수의 합 :" + odd);
 
-}
+		// 7!(7 * 6 * 5 * 4 * 3 * 2 * 1)
+		sum = 1;
+		for (int i = 7; i > 0; i--) {
+			sum = sum * i ;
+			
+		}
+		System.out.println("7 ! " + sum);
 	}
 }
